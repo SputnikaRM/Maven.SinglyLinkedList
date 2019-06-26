f81698b8820a87e44c2dd6de4ac36c8da6cfd51f
sputnika <sputnika91@gmail.com>
2019-06-26 16:32:10 -0400

    update

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,24 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
+
+
+
 public class SinglyLinkedList {
+
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+        Node (int data) {this.data=data;}
+    }
+
+    public class LinkedList {
+
+
+    }
+
+
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,17 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import jdk.nashorn.internal.ir.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    Node head;
+
+   static class Node {
+        int number;
+        Node next;
+
+
+    }
 }
